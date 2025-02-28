0c86ef73f80dabc0791aca79bb03a4211b496cb6
AGrishkov <AGrishkov@egartech.ru>
2025-02-28 20:33:17 +0300

    init

---
+++ src/test/java/ru/alexandr/sbertest/SberTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexandr.sbertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SberTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
